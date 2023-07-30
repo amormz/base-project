dd5081870e0ac5a738460b01118443c7cdfe8772
李子木 <zimu.li@renren-inc.com>
2023-07-30 23:37:14 +0800

    feat: stream util

--- base-project-util/src/main/java/pers/zymir/util/stream/StreamUtil.java
+++ base-project-util/src/main/java/pers/zymir/util/stream/StreamUtil.java
@@ -3,7 +3,9 @@ package pers.zymir.util.stream;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class StreamUtil {
   public static <T> List<T> nonNull(Collection<T> collection) {
@@ -12,5 +14,14 @@ public class StreamUtil {
 
   public static <T, R> List<R> mapTo(Collection<T> collection, Function<T, R> function) {
     return collection.stream().map(function).toList();
+  }
+
+  public static <T> List<T> filter(Collection<T> collection, Predicate<T> predicate) {
+    return collection.stream().filter(predicate).toList();
+  }
+
+  public static <T> List<T> foreachPeek(Collection<T> collection, Consumer<T> consumer) {
+    collection.forEach(consumer);
+    return collection.stream().toList();
   }
 }
